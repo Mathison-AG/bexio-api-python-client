cd052bef3894e4662ffef7e62e7be96ff4b2f4ae
Özer S <o.sahin@oesah.de>
2017-12-14 18:13:25 +0100

    fixed test urls.py

--- tests/urls.py
+++ tests/urls.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import url, include
 
-from bexiopy.urls import urlpatterns as bexiopy_urls
-
 urlpatterns = [
-    url(r'^', include(bexiopy_urls, namespace='bexiopy')),
+    url(r'bexiopy/', include('bexiopy.urls', namespace='bexiopy')),
 ]
